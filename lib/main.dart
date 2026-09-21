169dff630c01b78dab7d32bc12fc95b715c6c6d1
shuyamail2023-svg <shuyamail2023@gmail.com>
2026-09-21 16:08:02 +0200

    Change image asset in main.dart
    更换完成页纯背景图

--- lib/main.dart
+++ lib/main.dart
@@ -1066,7 +1066,7 @@ class CompletionPage extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           Image.asset(
-            '7241CC18-F9EC-43BE-BB04-A29E72E37882.png',
+            '92DF6262-13E6-4D92-B27E-F2DF355D8C54.png',
             fit: BoxFit.cover,
           ),
 
