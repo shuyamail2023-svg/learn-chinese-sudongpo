bc38698812f2fea795800271733f38c8a01829e0
shuyamail2023-svg <shuyamail2023@gmail.com>
2026-09-24 12:49:27 +0200

    Add conditional for poemIndex 7 in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -524,7 +524,8 @@ late final String poem = widget.poemIndex == 0
     : widget.poemIndex == 6
     ? '山下兰芽短浸溪，松间沙路净无泥，萧萧暮雨子规啼。 \n'
       '谁道人生无再少？门前流水尚能西！休将白发唱黄鸡。'
-    : '横看成岭侧成峰，\n'
+    : widget.poemIndex == 7
+    ? '横看成岭侧成峰，\n'
       '远近高低各不同。\n'
       '不识庐山真面目，\n'
       '只缘身在此山中。';
